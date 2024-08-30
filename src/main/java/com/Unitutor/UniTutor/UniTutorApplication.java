2fabfc55ba2b3c0dd3660074ffec3a9bdf00f1d8
Mateus Sanchez <mateo705@hotmail.com>
2024-08-30 17:31:08 -0500

    project start

---
+++ src/main/java/com/Unitutor/UniTutor/UniTutorApplication.java
@@ -0,0 +1,13 @@
+package com.Unitutor.UniTutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniTutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniTutorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Unitutor/UniTutor/UniTutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Unitutor.UniTutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniTutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
