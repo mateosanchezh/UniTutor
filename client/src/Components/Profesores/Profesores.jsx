2606b9bdfe4d49f6d4e8b64ca16cf1915cbfc91e
Jsilva0612 <jesussilva062701@gmail.com>
2024-09-08 16:55:12 -0500

    Primer commit

---
+++ client/src/Components/Profesores/Profesores.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Profesores = () => {
+  return (
+    <div>Profesores</div>
+  )
+}
+
+export default Profesores
---
+++ client/src/Components/Web_oficial/Web_oficial.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Web_oficial = () => {
+  return (
+    <div>Web_oficial</div>
+  )
+}
+
+export default Web_oficial
