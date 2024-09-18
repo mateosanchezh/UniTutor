9047e5a3b4788d8ee6643e519f881da252054e5a
Silva <jesussilva062701@gmail.com>
2024-09-18 14:11:00 -0500

    Se agrega el metodo para acceder al navbar desde dispositivos moviles

--- client/src/Components/Home/Navbar.jsx
+++ client/src/Components/Home/Navbar.jsx
@@ -1,37 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
-import { FaUser } from "react-icons/fa";
-
+import { FaUser, FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
-  return (
-   
-    <nav className="navbar">
+  const [isOpen, setIsOpen] = useState(false);
+  const navRef = useRef(null);
 
-      <div className='icono'>
-        <FaUser className='user' />
-        </div>
-      <ul>
-      
-     
-      <li>
-          <Link to="/">INICIO</Link>
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    const closeMenu = (e) => {
+      if (isOpen && navRef.current && !navRef.current.contains(e.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', closeMenu);
+    return () => document.removeEventListener('mousedown', closeMenu);
+  }, [isOpen]);
+
+  return (
+    <nav className="navbar" ref={navRef}>
+      <div className='menu-icon' onClick={toggleMenu}>
+        {isOpen ? <FaTimes /> : <FaBars />}
+      </div>
+      <ul className={isOpen ? 'nav-links open' : 'nav-links'}>
+        <li>
+          <Link to="/" onClick={() => setIsOpen(false)}>INICIO</Link>
         </li>
         <li>
-          <Link to="/asignaturas">ASIGNATURAS</Link>
+          <Link to="/asignaturas" onClick={() => setIsOpen(false)}>ASIGNATURAS</Link>
         </li>
         <li>
-          <Link to="/profesores">PROFESORES</Link>
+          <Link to="/profesores" onClick={() => setIsOpen(false)}>PROFESORES</Link>
         </li>
         <li>
-          <Link to="/web">WEB OFICIAL</Link>
+          <Link to="/web" onClick={() => setIsOpen(false)}>WEB OFICIAL</Link>
         </li>
         <li>
-          <Link to="#">IDIOMA</Link>
+          <Link to="#" onClick={() => setIsOpen(false)}>IDIOMA</Link>
         </li>
       </ul>
-        
-     
+      <div className='icono'>
+        <FaUser className='user' />
+      </div>
     </nav>
   );
 };
