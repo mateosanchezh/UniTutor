ea09692b496235300d99c40b0de7034010717007
Mateus Sanchez <mateo705@hotmail.com>
2024-09-17 22:30:05 -0500

    feat: se añade repostory de Login

---
+++ Backend/src/main/java/com/Unitutor/UniTutor/repository/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package com.Unitutor.UniTutor.repository;
+
+import com.Unitutor.UniTutor.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Usuario findByEmail(String email);
+}
