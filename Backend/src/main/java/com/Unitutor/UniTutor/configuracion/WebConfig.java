7c0d3570be9dc51023de2198d24fd787a9d6236f
Mateus Sanchez <mateo705@hotmail.com>
2024-09-18 12:43:03 -0500

    update: se adapta para recibir la configuracion de Cors que permite el llamado de datos desde el font

--- Backend/src/main/java/com/Unitutor/UniTutor/configuracion/WebConfig.java
+++ Backend/src/main/java/com/Unitutor/UniTutor/configuracion/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@NotNull CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // Cambia esto según tu URL
+                .allowedOrigins("http://localhost:5174") // Updated to match your frontend URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
