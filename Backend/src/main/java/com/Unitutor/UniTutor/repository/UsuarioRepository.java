c2440076d185936e84dfc32c372f52421ebbceac
Mateus Sanchez <mateo705@hotmail.com>
2024-10-01 16:32:50 -0500

    se cambia el metodo a traer pasa de ser email a usuario

--- Backend/src/main/java/com/Unitutor/UniTutor/repository/UsuarioRepository.java
+++ Backend/src/main/java/com/Unitutor/UniTutor/repository/UsuarioRepository.java
@@ -4,5 +4,5 @@ import com.Unitutor.UniTutor.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Usuario findByEmail(String email);
+    Usuario findByUser(String user);
 }
