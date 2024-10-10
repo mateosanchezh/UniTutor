03eb8af1830c5d5000e8686c133563457f8e0a83
Mateus Sanchez <mateo705@hotmail.com>
2024-10-10 00:16:09 -0500

    add: se añade para proteger los datos

---
+++ Backend/src/main/java/com/Unitutor/UniTutor/DTO/TutorMateriaSemestreDTO.java
@@ -0,0 +1,43 @@
+package com.Unitutor.UniTutor.DTO;
+
+public class TutorMateriaSemestreDTO {
+
+    private Long id;
+    private Long tutorId;
+    private Long materiaId;
+    private Integer semestre;
+
+    // Getters y setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getTutorId() {
+        return tutorId;
+    }
+
+    public void setTutorId(Long tutorId) {
+        this.tutorId = tutorId;
+    }
+
+    public Long getMateriaId() {
+        return materiaId;
+    }
+
+    public void setMateriaId(Long materiaId) {
+        this.materiaId = materiaId;
+    }
+
+    public Integer getSemestre() {
+        return semestre;
+    }
+
+    public void setSemestre(Integer semestre) {
+        this.semestre = semestre;
+    }
+}
