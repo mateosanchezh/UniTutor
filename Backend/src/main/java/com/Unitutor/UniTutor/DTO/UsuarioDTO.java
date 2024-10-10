7981581e6552631f49f84565d57cd8ca37f87a39
Mateus Sanchez <mateo705@hotmail.com>
2024-10-10 00:11:59 -0500

    refactor: se agrego TutorMateriaSemestreDTO

--- Backend/src/main/java/com/Unitutor/UniTutor/DTO/UsuarioDTO.java
+++ Backend/src/main/java/com/Unitutor/UniTutor/DTO/UsuarioDTO.java
@@ -3,6 +3,8 @@ package com.Unitutor.UniTutor.DTO;
 import com.Unitutor.UniTutor.model.enums.UserEstadoCuenta;
 import com.Unitutor.UniTutor.model.enums.UserRole;
 
+import java.util.Set;
+
 public class UsuarioDTO {
 
     private Long id;
@@ -16,6 +18,65 @@ public class UsuarioDTO {
     private UserEstadoCuenta userEstadoCuenta;
     private Long carreraId;
     private Integer semestre;
+    private Set<TutorMateriaSemestreDTO> materiasAsignadas;  // Relación con materias y semestre
+
+    // Getters y setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public UserRole getUserRole() {
         return userRole;
@@ -33,20 +94,12 @@ public class UsuarioDTO {
         this.userEstadoCuenta = userEstadoCuenta;
     }
 
-    public String getUser() {
-        return user;
+    public Long getCarreraId() {
+        return carreraId;
     }
 
-    public void setUser(String user) {
-        this.user = user;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
+    public void setCarreraId(Long carreraId) {
+        this.carreraId = carreraId;
     }
 
     public Integer getSemestre() {
@@ -57,51 +110,11 @@ public class UsuarioDTO {
         this.semestre = semestre;
     }
 
-    public String getPassword() {
-        return password;
+    public Set<TutorMateriaSemestreDTO> getMateriasAsignadas() {
+        return materiasAsignadas;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Long getCarreraId() {
-        return carreraId;
-    }
-
-    public void setCarreraId(Long carreraId) {
-        this.carreraId = carreraId;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
+    public void setMateriasAsignadas(Set<TutorMateriaSemestreDTO> materiasAsignadas) {
+        this.materiasAsignadas = materiasAsignadas;
     }
 }
